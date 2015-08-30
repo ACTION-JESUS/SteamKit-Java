a3a25551b34c9e6046e2e0a9fcfcfe8155987a31
ACTION JESUS <jesus@development-vm>
2015-08-30 14:39:38 -0700

    Removed unused import

--- src/main/java/uk/co/thomasc/steamkit/steam3/webapi/WebAPI.java
+++ src/main/java/uk/co/thomasc/steamkit/steam3/webapi/WebAPI.java
@@ -1,12 +1,10 @@
 package uk.co.thomasc.steamkit.steam3.webapi;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
